641579ee8dc6cdf3957c3eb562ba65cfdfa662c4
cnwangxiao <wangxiao@turingvideo.net>
2023-05-12 17:52:18 +0800

    add jitpack

---
+++ custom_setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+VERSION=t0.9.0-android
+
+RELEASE_VERSION=${VERSION}
+
+SDK_BIN_URL=https://github.com/turingvideo/ijkplayer-for-flutter/releases/download/${VERSION}/ijkplayer-full64.aar
+
+BUILD_DIR=build
+mkdir -p ${BUILD_DIR}
+cd ${BUILD_DIR}
+
+echo "Downloading ijkplayer ${VERSION} binary for android."
+curl -L -O ${SDK_BIN_URL}
+
+mvn install:install-file \
+    -Dfile=ijkplayer-full64.aar \
+    -Dpackaging=aar \
+    -Dversion=${RELEASE_VERSION} \
+    -DgroupId=com.github.turingvideo \
+    -DartifactId=android
